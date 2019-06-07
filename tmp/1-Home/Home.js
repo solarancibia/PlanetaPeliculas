847c3cff121cacb12bc05f90edd62891d6ef70ed
Juan Ignacio <jbedoian@udesa.edu.ar>
2019-06-07 16:08:37 -0300

    porongaaaa

--- tmp/1-Home/Home.js
+++ tmp/1-Home/Home.js
@@ -1,3 +1,15 @@
-var nombre= prompt ("Ingrese su nombre")
-var reemplazo = document.querySelector("p.saludo")
-reemplazo.innerText= "Bienvenid@" + "" + nombre
+window.addEventListener("load", function() {}
+
+  fetch("URL")
+    .then(function(respuesta) {
+      return respuesta.json()
+    })
+    .then(function(informacion) {
+      var peliculas = informacion.???????
+
+      for (var i = 0; i < 6; i++) {
+        document.querySelector("CONTENEDOR").innerHTML += '<img class="posters" src="' + ALGOOO + '" alt="">'
+      }
+    })
+
+)
