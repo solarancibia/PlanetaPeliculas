5e677706e381e1d3c1380d41f221d44288b67b0e
Sol <srodriguezarancibia@udesa.edu.ar>
2019-06-07 16:18:17 -0300

    hols

--- tmp/1-Home/Home.js
+++ tmp/1-Home/Home.js
@@ -1,19 +1,3 @@
-window.addEventListener("load", function(){
 var nombre= prompt ("Ingrese su nombre")
 var reemplazo = document.querySelector("p.saludo")
-reemplazo.innerText= "Bienvenid@" + "" + nombre
-
-fetch("URL")
-    .then(function(respuesta) {
-      return respuesta.json()
-    })
-    .then(function(informacion) {
-      var peliculas = informacion.???????
-
-      for (var i = 0; i < 6; i++) {
-        document.querySelector("CONTENEDOR").innerHTML += '<img class="posters" src="' + ALGOOO + '" alt="">'
-      }
-    })
-
-)
-}
+reemplazo.innerText= "Bienvenid@" + " " + nombre
