6b0fd2fc26da8ce4bce5e25e1611647db459c98d
meh889 <matiasgallardo889@gmail.com>
2019-06-08 14:03:36 -0300

    sigue sin funcar

--- tmp/2-Generos/ListadoDeGeneros.js
+++ tmp/2-Generos/ListadoDeGeneros.js
@@ -1,9 +1,13 @@
 window.onload = function(){
-   document.querySelector(".logincompleto").onSubmit = function (yey){
-     var nombre = document.querySelector(".name")
-     document.querySelector(".saludo").innerHTML = "Welcome" + nombre
+   document.querySelector("form.logincompleto").onSubmit = function (yey){
+     var nombre = document.querySelector("label.name")
+     var reemplazo = document.querySelector("p.saludo")
+     reemplazo.innerText = "Welcome" + " " + nombre
    }
 
 
+  // var nombre= prompt ("Ingrese su nombre")
+  // var reemplazo = document.querySelector("p.saludo")
+   //reemplazo.innerText= "Bienvenid@" + " " + nombre
 
 }
