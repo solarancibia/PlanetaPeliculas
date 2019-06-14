99c9b220dfc822c58547e8e220081f7cbb88182d
Juani <jbedoian@udesa.edu.ar>
2019-06-14 17:43:08 -0300

    ccc

---
+++ tmp/3-Pelis por genero/peliculasPorGenero.js
@@ -0,0 +1,35 @@
+window.onload = function(){
+
+  var paramsString = "idGenero=28"
+  var searchParams = new URLSearchParams(paramsString);
+  //ACtion
+  if(searchParams.has("idGenero")){
+    fetch("https://api.themoviedb.org/3/discover/movie?api_key=d72b8119ca0d802447ebd91bded10750&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_genres=28")
+        .then(function(respuesta) {
+         return respuesta.json()
+       })
+        .then(function(informacion) {
+         var peliculas = informacion.results
+         console.log(informacion)
+
+         for (var i = 0; i < 10 ; i++) { //informacion.results.length
+           console.log('esto es informacion' + peliculas);
+           var titulo = peliculas[i].title
+           var url = 'https://image.tmdb.org/t/p/w500/'
+           var img =  peliculas[i].poster_path;
+           var id = peliculas[i].id
+
+          var li;
+          li = '<li>'
+          li += '<img src=' + url + img + '>'
+          li += '<div class="uk-position-center uk-panel"><h1>' + titulo + '</h1></div>'
+          li += '</li>'
+
+          console.log(li);
+
+          document.querySelector(".ul-genero").innerHTML += li
+         }
+        })
+  }
+
+}
