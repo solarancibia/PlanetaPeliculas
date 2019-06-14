39904e3e22a935d0cdd85fd6db11ac1b2da089e2
meh889 <mgallardo@udesa.edu.ar>
2019-06-14 14:23:18 -0300

    login que falta pero ya hace cosas lo cual me hace feliz

--- tmp/2-Generos/ListadoDeGeneros.js
+++ tmp/2-Generos/ListadoDeGeneros.js
@@ -1,13 +1,15 @@
 window.onload = function(){
-   document.querySelector("form.logincompleto").onSubmit = function (yey){
-     var nombre = document.querySelector("label.name")
+   document.querySelector("form.logincompleto").onsubmit = function (yey){
+     var nombre = document.querySelector("input.name").value
      var reemplazo = document.querySelector("p.saludo")
      reemplazo.innerText = "Welcome" + " " + nombre
+     yey.preventDefault()
+     document.querySelector(".uk-modal-close").click()
+     sessionStorage.setItem("Usuario", nombre)
+
    }
 
-
-  // var nombre= prompt ("Ingrese su nombre")
-  // var reemplazo = document.querySelector("p.saludo")
-   //reemplazo.innerText= "Bienvenid@" + " " + nombre
-
+  if (sessionStorage.getItem("Usuario") != null) {
+    document.querySelector("a#login").style.display = "none"
+  }
 }
