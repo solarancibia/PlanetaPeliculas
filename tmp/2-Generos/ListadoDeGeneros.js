08ef3ad97a9d51f03ed29cd212d5cdf566ca4797
meh889 <mgallardo@udesa.edu.ar>
2019-06-14 17:21:36 -0300

    login separado

--- tmp/2-Generos/ListadoDeGeneros.js
+++ tmp/2-Generos/ListadoDeGeneros.js
@@ -1,38 +1,5 @@
 window.onload = function(){
 
-// che, hay algo en sesion????
-if (sessionStorage.getItem("Usuario") != null) {
-  // si hay data, mostrala
-  document.querySelector("a#login").style.display = "none"
-  welcomeUser()
-}
-// no hay data, no hago nada
-
-
-   document.querySelector("form.logincompleto").onsubmit = function (yey){
-     welcomeUser()
-     yey.preventDefault()
-     document.querySelector(".uk-modal-close").click()
-     var nombre = document.querySelector("input.name").value
-     sessionStorage.setItem("Usuario", nombre)
-
-   }
-
-  if (sessionStorage.getItem("Usuario") != null) {
-    document.querySelector("a#login").style.display = "none"
-  }
-}
-
-function welcomeUser(){
-  var nombre = document.querySelector("input.name").value
-  console.log(nombre);
-  if (nombre.length===0) {
-    var nombre = sessionStorage.getItem("Usuario")
-  }
-
-  var reemplazo = document.querySelector("p.saludo")
-  reemplazo.innerText = "Welcome" + " " + nombre
-}
 
   // var nombre= prompt ("Ingrese su nombre")
   // var reemplazo = document.querySelector("p.saludo")
@@ -60,4 +27,4 @@ function welcomeUser(){
      })
      .catch(function(error) {
        console.log("Error: " + error);
-     })
+     })}
---
+++ tmp/Login/LoginCompleto.js
@@ -0,0 +1,35 @@
+window.addEventListener("load",function() {
+  // che, hay algo en sesion????
+  if (sessionStorage.getItem("Usuario") != null) {
+    // si hay data, mostrala
+    document.querySelector("a#login").style.display = "none"
+    welcomeUser()
+  }
+  // no hay data, no hago nada
+
+
+     document.querySelector("form.logincompleto").onsubmit = function (yey){
+       welcomeUser()
+       yey.preventDefault()
+       document.querySelector(".uk-modal-close").click()
+       var nombre = document.querySelector("input.name").value
+       sessionStorage.setItem("Usuario", nombre)
+
+     }
+
+    if (sessionStorage.getItem("Usuario") != null) {
+      document.querySelector("a#login").style.display = "none"
+    }
+
+
+  function welcomeUser(){
+    var nombre = document.querySelector("input.name").value
+    console.log(nombre);
+    if (nombre.length===0) {
+      var nombre = sessionStorage.getItem("Usuario")
+    }
+
+    var reemplazo = document.querySelector("p.saludo")
+    reemplazo.innerText = "Welcome" + " " + nombre
+  }
+})
