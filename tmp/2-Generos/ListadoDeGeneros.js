6e572b6ac4e828454b47f34aea60d0746922b2d7
meh889 <mgallardo@udesa.edu.ar>
2019-06-14 16:02:37 -0300

    el login funcaaaaa

--- tmp/2-Generos/ListadoDeGeneros.js
+++ tmp/2-Generos/ListadoDeGeneros.js
@@ -1,10 +1,19 @@
 window.onload = function(){
+
+// che, hay algo en sesion????
+if (sessionStorage.getItem("Usuario") != null) {
+  // si hay data, mostrala
+  document.querySelector("a#login").style.display = "none"
+  welcomeUser()
+}
+// no hay data, no hago nada
+
+
    document.querySelector("form.logincompleto").onsubmit = function (yey){
-     var nombre = document.querySelector("input.name").value
-     var reemplazo = document.querySelector("p.saludo")
-     reemplazo.innerText = "Welcome" + " " + nombre
+     welcomeUser()
      yey.preventDefault()
      document.querySelector(".uk-modal-close").click()
+     var nombre = document.querySelector("input.name").value
      sessionStorage.setItem("Usuario", nombre)
 
    }
@@ -12,4 +21,15 @@ window.onload = function(){
   if (sessionStorage.getItem("Usuario") != null) {
     document.querySelector("a#login").style.display = "none"
   }
+}
+
+function welcomeUser(){
+  var nombre = document.querySelector("input.name").value
+  console.log(nombre);
+  if (nombre.length===0) {
+    var nombre = sessionStorage.getItem("Usuario")
+  }
+
+  var reemplazo = document.querySelector("p.saludo")
+  reemplazo.innerText = "Welcome" + " " + nombre
 }
