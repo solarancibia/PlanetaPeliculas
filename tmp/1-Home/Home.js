d4dce56b9ca5a23a402c06b3d8dc6c9c628fac76
Juani <jbedoian@udesa.edu.ar>
2019-06-14 16:08:36 -0300

    Arreglamos el home.js

--- tmp/1-Home/Home.js
+++ tmp/1-Home/Home.js
@@ -1,8 +1,4 @@
 window.addEventListener("load", function(){
-var nombre= prompt ("Ingrese su nombre")
-var reemplazo = document.querySelector("p.saludo")
-reemplazo.innerText= "Bienvenid@" + " " + nombre
-
 
 //API DE LA PELICULAS POPULARES
 fetch("https://api.themoviedb.org/3/trending/all/day?api_key=d72b8119ca0d802447ebd91bded10750")
@@ -11,10 +7,7 @@ fetch("https://api.themoviedb.org/3/trending/all/day?api_key=d72b8119ca0d802447e
    })
     .then(function(informacion) {
      var peliculas = informacion.results
-<<<<<<< HEAD
-=======
      console.log(informacion)
->>>>>>> master
 
      for (var i = 0; i < 6 ; i++) { //informacion.results.length
        console.log('esto es informacion' + peliculas);
@@ -22,30 +15,6 @@ fetch("https://api.themoviedb.org/3/trending/all/day?api_key=d72b8119ca0d802447e
        var url = 'https://image.tmdb.org/t/p/w500/'
        var img =  peliculas[i].poster_path;
        var id = peliculas[i].id
-<<<<<<< HEAD
-      document.querySelector("ul.uk-slider-items uk-child-width-1-5").innerHTML += '<li>img src=""https://image.tmdb.org/t/p/original/' + img + '"alt="" >'"</li>"
-
-
-     }
-  })
-)
-}
-
-
-fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=d72b8119ca0d802447ebd91bded10750&language=en-US&page=1")
-    .then(function(respuesta) {
-     return respuesta.json()
-   })
-    .then(function(informacion) {
-     var peliculas2 = informacion.data
-
-     for (var i = 0; i < 6; i++) {
-       var titulo = peliculas2[i].title
-       var img = peliculas2[i].poster_path
-       var id = peliculas2[i].id
-       var rated = peliculas2[i].vote_average
-      document.querySelector("ul.uk-slider-items uk-child-width-1-5").innerHTML += '<img class="posters" src="https://image.tmdb.org/t/p/original/' + img + '" alt="">'
-=======
 
       var div;
       div += '<li>'
@@ -56,7 +25,6 @@ fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=d72b8119ca0d802447eb
       console.log(div);
 
       document.querySelector("#listaPopulares").innerHTML += div
->>>>>>> master
      }
     })
 
--- tmp/2-Generos/ListadoDeGeneros.js
+++ tmp/2-Generos/ListadoDeGeneros.js
@@ -9,5 +9,28 @@ window.onload = function(){
   // var nombre= prompt ("Ingrese su nombre")
   // var reemplazo = document.querySelector("p.saludo")
    //reemplazo.innerText= "Bienvenid@" + " " + nombre
+   fetch("https://api.themoviedb.org/3/genre/movie/list?api_key=d72b8119ca0d802447ebd91bded10750&language=en-US")
+     .then(function(respuesta) {
+       return respuesta.json()
+     })
+     .then(function(informacion) {
+       var generos = informacion.genres
+       console.log(generos);
+       for (var i = 0; i < generos.length; i++) {
+         var nombre =  generos[i].name
+         var id = generos[i].id
 
-}
+
+         var div = "";
+         div += '<li>'
+         div += '<div class="uk-position-center uk-panel"><h1>' + nombre + '</h1></div>'
+         div += '</li>'
+
+
+         document.querySelector("#generos").innerHTML += div
+       }
+     })
+     .catch(function(error) {
+       console.log("Error: " + error);
+     })
+ }
