2525aeb2947e2a7911cca68bf00da8f61659423b
juani <jbedoian@udesa.edu.ar>
2019-06-18 11:15:36 -0300

    Arreglamos DEVUELTA un problema

--- tmp/1-Home/Home.js
+++ tmp/1-Home/Home.js
@@ -55,6 +55,8 @@ fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=d72b8119ca0d802447eb
 
       var li;
       li = '<li>'
+      li += "<button class='estrellita'> &#9733; </button>"
+      li += "<a href='../5-Detalle de una pelicula/detallePeli.html?idDePeli="+id+"'>"
       li += '<img src=' + url + img + '>'
       li += '<div class="uk-position-center uk-panel"><h1>' + titulo + '</h1></div>'
       li += '</li>'
@@ -86,6 +88,8 @@ fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=d72b8119ca0d802447eb
 
        var li;
        li = '<li>'
+      li += "<button class='estrellita'> &#9733; </button>"
+       li += "<a href='../5-Detalle de una pelicula/detallePeli.html?idDePeli="+id+"'>"
        li += '<img src=' + url + img + '>'
        li += '<div class="uk-position-center uk-panel"><h1>' + titulo + '</h1></div>'
        li += '</li>'
