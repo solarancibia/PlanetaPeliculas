0b3dcfee8f65bc4c9b54bbd4b5f538ccc8c755bf
Sol <srodriguezarancibia@udesa.edu.ar>
2019-06-18 16:20:33 -0300

    titulos arreglados

--- tmp/1-Home/Home.js
+++ tmp/1-Home/Home.js
@@ -21,7 +21,7 @@ fetch("https://api.themoviedb.org/3/trending/all/day?api_key=d72b8119ca0d802447e
       li += "<button class='estrellita'> &#9733; </button>"
       li += "<a href='../5-Detalle de una pelicula/detallePeli.html?idDePeli="+id+"'>"
       li += '<img src=' + url + img + '>'
-      li += '<div class="uk-position-center uk-panel"><h1>' + titulo + '</h1></div>'
+      li += '<div class="uk-position-center uk-panel divPelis"><h1 class="headersHome">' + titulo + '</h1></div>'
       li += "</a>"
       li += '</li>'
 
@@ -58,7 +58,7 @@ fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=d72b8119ca0d802447eb
       li += "<button class='estrellita'> &#9733; </button>"
       li += "<a href='../5-Detalle de una pelicula/detallePeli.html?idDePeli="+id+"'>"
       li += '<img src=' + url + img + '>'
-      li += '<div class="uk-position-center uk-panel"><h1>' + titulo + '</h1></div>'
+      li += '<div class="uk-position-center uk-panel divPelis"><h1 class="headersHome">' + titulo + '</h1></div>'
       li += '</li>'
 
       console.log(li);
@@ -91,7 +91,7 @@ fetch("https://api.themoviedb.org/3/movie/top_rated?api_key=d72b8119ca0d802447eb
       li += "<button onclick='agregarFavoritos()' class='estrellita'> &#9733; </button>"
        li += "<a href='../5-Detalle de una pelicula/detallePeli.html?idDePeli="+id+"'>"
        li += '<img src=' + url + img + '>'
-       li += '<div class="uk-position-center uk-panel"><h1>' + titulo + '</h1></div>'
+       li += '<div class="uk-position-center uk-panel divPelis"><h1 class= "headersHome">' + titulo + '</h1></div>'
        li += '</li>'
 
        console.log(li);
